5f938428d8399f24d70f141cc3a503e20185da86
Diego Andrés Toapanta <admaigualca@gmail.com>
2021-02-04 15:20:11 -0500

    First commit}

---
+++ src/main/java/com/platziEspe/platziEspe/PlatziEspeApplication.java
@@ -0,0 +1,13 @@
+package com.platziEspe.platziEspe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziEspeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziEspeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platziEspe/platziEspe/PlatziEspeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platziEspe.platziEspe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziEspeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
